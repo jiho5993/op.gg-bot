7cb7a6cd1ff6a5dd928c52fa07c9ed5499f4492f
박지호 <jiho5993@naver.com>
2020-12-24 21:08:31 +0900

    feat: Add match command

--- Summoner.py
+++ Summoner.py
@@ -1,16 +1,60 @@
 class Summoner:
-    level = ""
+    level = -1
     solo_rank = ""
     sub_rank = ""
+    matchList = []
 
     def __init__(self, name):
         self.name = name
 
-    def get_level(self, bs):
+    def get_summoner_info(self, bs):
+        self.__get_level(bs)
+        self.__get_rank(bs)
+
+    def get_match_list(self, bs):
+        match_list = bs.select("div.Content > div.GameItemList > div.GameItemWrap")
+        for match in match_list:
+            winlose = match.select("div.GameResult")[0]
+            champ_name = match.select("div.GameSettingInfo > div.ChampionName > a")[0]
+            self.matchList.append({
+                "Win": winlose.text.strip(),
+                "ChampionName": champ_name.text.strip()
+            })
+
+    def msg_summoner_info(self):
+        result = '{}\n' \
+                 'level: {}\n' \
+                 'solo rank: {} ({})\n' \
+                 'sub rank: {} ({})' \
+            .format(self.name,
+                    self.level,
+                    self.solo_rank["tier"],
+                    self.solo_rank["point"],
+                    self.sub_rank["tier"],
+                    self.sub_rank["point"],
+                    )
+        return result
+
+    def msg_match_list(self):
+        result, count = "", 1
+        for match in self.matchList:
+            result += str(count) + ' : ' \
+                      + str(match["ChampionName"]) + ' ' \
+                      + str("승리" if match["Win"] == "Victory" else "패배") \
+                      + '\n'
+            count += 1
+        self.matchList.clear()
+        return result
+
+    def __get_level(self, bs):
         user_level = bs.select("div.ProfileIcon > span.Level")[0]
         self.level = user_level.text.strip()
 
-    def get_solo_rank(self, bs):
+    def __get_rank(self, bs):
+        self.__get_solo_rank(bs)
+        self.__get_sub_rank(bs)
+
+    def __get_solo_rank(self, bs):
         try:
             tier = bs.select("div.TierRank")[0]
             point = bs.select("div.TierInfo > span.LeaguePoints")[0]
@@ -24,7 +68,7 @@ class Summoner:
                 "point": "0LP"
             }
 
-    def get_sub_rank(self, bs):
+    def __get_sub_rank(self, bs):
         try:
             tier = bs.select("div.sub-tier__rank-tier")[0]
             point = bs.select("div.sub-tier__info > div.sub-tier__league-point")[0]
@@ -39,7 +83,3 @@ class Summoner:
                 "tier": "Unranked",
                 "point": "0LP"
             }
-
-    def get_rank(self, bs):
-        self.get_solo_rank(bs)
-        self.get_sub_rank(bs)
--- main.py
+++ main.py
@@ -1,56 +1,65 @@
 import telepot
 import requests
+import sys
 from Summoner import Summoner
 from bs4 import BeautifulSoup
 from pprint import pprint
 
-TOKEN = ''
+TOKEN = sys.argv[1]
 
-def get_user_info(summoner_name):
+def get_info(summoner_name, type):
     url = 'https://www.op.gg/summoner/userName={}'.format(summoner_name)
     r = requests.get(url)
     bs = BeautifulSoup(r.text, "html.parser")
 
-    # get summoner info
-    try:
-        user = Summoner(summoner_name)
-        user.get_level(bs)
-        user.get_rank(bs)
-        return user
-    except:
-        return False
+    user = Summoner(summoner_name)
 
-def make_message(summoner):
-    if summoner is not False:
-        result = '{}\n' \
-                 'level: {}\n' \
-                 'solo rank: {} ({})\n' \
-                 'sub rank: {} ({})' \
-            .format(summoner.name,
-                    summoner.level,
-                    summoner.solo_rank["tier"],
-                    summoner.solo_rank["point"],
-                    summoner.sub_rank["tier"],
-                    summoner.sub_rank["point"],
-                    )
-        return result
-    return "This user is not registered."
+    # get summoner info
+    if type == 1:
+        try:
+            user.get_summoner_info(bs)
+            return user
+        except:
+            return None
+    # get match list
+    elif type == 2:
+        try:
+            user.get_match_list(bs)
+            return user
+        except:
+            return None
 
 def handler(msg):
     content_type, chat_type, chat_id, msg_date, msg_id = telepot.glance(msg, long=True)
     pprint(msg)
+    print('-'*100)
 
     if content_type == 'text':
         str_msg = msg['text']
+        # /이면 명령 실행
         if str_msg[0:1] == '/':
             args = str_msg.split(" ")
             command = args[0]
             del args[0]
 
+            bot.sendMessage(chat_id, "plz wait...")
+
             if command == '/user':
                 name = ' '.join(args)
-                result = make_message(get_user_info(name))
-                bot.sendMessage(chat_id, result)
+                summoner = get_info(name, 1)
+                if summoner is not None:
+                    bot.sendMessage(chat_id, summoner.msg_summoner_info())
+                else:
+                    bot.sendMessage(chat_id, "This user is not registered.")
+            elif command == '/match':
+                name = ' '.join(args)
+                summoner = get_info(name, 2)
+                if summoner is not None:
+                    bot.sendMessage(chat_id, summoner.msg_match_list())
+                else:
+                    bot.sendMessage(chat_id, "This user is not registered.")
+
+
 
 bot = telepot.Bot(TOKEN)
 bot.message_loop(handler, run_forever=True)
